e9ef34509d24885b2d21b83027307f2e10a59e15
Rubanov Andrey <nirwash@bk.ru>
2023-01-09 20:00:47 +0300

    provide instances

--- app/src/main/java/com/nirwashh/android/mynumbertask/main/sl/Core.kt
+++ app/src/main/java/com/nirwashh/android/mynumbertask/main/sl/Core.kt
@@ -13,24 +13,18 @@ interface Core : CloudModule, CacheModule, ManageResources {
 
     class Base(
         context: Context,
-        private val isRelease: Boolean
+        private val provideInstances: ProvideInstances
     ) : Core {
         private val manageResources = ManageResources.Base(context)
         private val dispatchersList by lazy {
             DispatchersList.Base()
         }
         private val cloudModule by lazy {
-            if (isRelease)
-                CloudModule.Base()
-            else
-                CloudModule.Mock()
+            provideInstances.provideCloudModule()
         }
 
         private val cacheModule by lazy {
-            if (isRelease)
-                CacheModule.Base(context)
-            else
-                CacheModule.Mock(context)
+            provideInstances.provideCacheModule()
         }
 
         override fun <T> service(clasz: Class<T>): T = cloudModule.service(clasz)
--- app/src/main/java/com/nirwashh/android/mynumbertask/main/sl/NumbersApp.kt
+++ app/src/main/java/com/nirwashh/android/mynumbertask/main/sl/NumbersApp.kt
@@ -12,12 +12,16 @@ class NumbersApp : Application(), ProvideViewModel {
 
     override fun onCreate() {
         super.onCreate()
+        val provideInstances = if (BuildConfig.DEBUG)
+            ProvideInstances.Mock(this)
+        else
+            ProvideInstances.Release(this)
         viewModelsFactory =
             ViewModelsFactory(
                 DependencyContainer.Base(
                     Core.Base(
                         this,
-                        isRelease = !BuildConfig.DEBUG
+                        provideInstances
                     )
                 )
             )
